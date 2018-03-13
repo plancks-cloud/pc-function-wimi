5c24d66658288ce614eafc70fcfeae12df08ecbe
Captain Justin <just1689@gmail.com>
2018-03-13 21:10:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 exports.handler = (req, res) => {  
-	const result = {
-		address: req.headers['x-forwarded-for']      
-    }
-    res.status(200).send(JSON.stringify(result));
+	res.status(200).send(JSON.stringify({
+		address: req.headers['x-forwarded-for']
+	}));
 };
 
